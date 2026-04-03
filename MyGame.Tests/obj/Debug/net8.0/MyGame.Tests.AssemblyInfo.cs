b6973232da2d893fbf06aea3ca34af630088a888
Peagle8 <sean_trevitt@hotmail.com>
2026-04-03 10:15:22 -0400

    Initial commit
    Initial commit

--- MyGame.Tests/obj/Debug/net8.0/MyGame.Tests.AssemblyInfo.cs
+++ MyGame.Tests/obj/Debug/net8.0/MyGame.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGame.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7214eda1228ba7ec0dc2c511eff3d5fd27cdd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGame.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGame.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
