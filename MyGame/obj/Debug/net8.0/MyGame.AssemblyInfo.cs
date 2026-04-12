2373d426e8dac6e8e926bf542c83cee07945e0d2
Peagle8 <sean_trevitt@hotmail.com>
2026-04-12 17:06:05 -0400

    Add to ignore

--- MyGame/obj/Debug/net8.0/MyGame.AssemblyInfo.cs
+++ MyGame/obj/Debug/net8.0/MyGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+313fbaf02a0624a1f31ea8724a02841571a84fcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2822a3bbafc3e43a38d27c3c4ba4e12245ca2e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
